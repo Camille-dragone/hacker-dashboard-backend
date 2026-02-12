738252e97b77c0515be51be30b787fe1f194d869
Camille <camillebujotzek02@gmail.com>
2026-02-12 11:42:46 +0100

    fix: add netlify to CORS

--- app/database.py
+++ app/database.py
@@ -1,13 +1,20 @@
+import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = "sqlite:///./hacker.db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False}  
-)
+if DATABASE_URL:
+    engine = create_engine(
+        DATABASE_URL,
+        pool_pre_ping=True,
+    )
+else:
+    engine = create_engine(
+        "sqlite:///./hacker.db",
+        connect_args={"check_same_thread": False},
+        pool_pre_ping=True,
+    )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
